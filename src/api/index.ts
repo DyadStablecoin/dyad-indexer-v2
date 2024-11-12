dad194d764f887b0713327f6027c7fdc18a8940c
Coffee ☕️ <coffee@coffeebot.dev>
2024-11-12 17:00:03 -0500

    override values in boosted size computation (#16)
    * override values in boosted size computation
    
    * check isnan
    
    * fix div by zero errors
    
    * fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -104,8 +104,8 @@ ponder.get('/api/yields/:id', async (context) => {
   const simLiquidity: undefined | string = context.req.query('liquidity');
 
   const simParameters = {
-    xp: simXp ? Number(simXp) : undefined,
-    liquidity: simLiquidity ? Number(simLiquidity) : undefined,
+    xp: simXp !== undefined ? Number(simXp) : undefined,
+    liquidity: simLiquidity !== undefined ? Number(simLiquidity) : undefined,
   };
 
   if (simParameters.xp !== undefined && isNaN(simParameters.xp)) {
@@ -223,14 +223,17 @@ async function getYieldsForPool(
     }
   }
 
-  const totalXpScaled = Number(formatUnits(totalXp, 27)) / totalParticipants;
+  const totalXpScaled =
+    totalParticipants > 0
+      ? Number(formatUnits(totalXp, 27)) / totalParticipants
+      : 0;
   const medianLiquidityScaled = median(
     lpSizes.map((n) => Number(formatUnits(n, 18))),
   );
 
   const noteBoostedSize = computeBoostedSize(
-    xpAmount,
-    amountDeposited,
+    xpToUse,
+    liquidityToUse,
     totalXpScaled,
     medianLiquidityScaled,
   );
@@ -251,7 +254,10 @@ async function getYieldsForPool(
   }
 
   const rewardPerSecondWad =
-    rewardRate * parseEther((noteBoostedSize / totalEffectiveSize).toString());
+    totalEffectiveSize > 0n
+      ? rewardRate *
+        parseEther((noteBoostedSize / totalEffectiveSize).toString())
+      : 0n;
 
   const rewardPerYear = (rewardPerSecondWad * 31536000n) / BigInt(1e18); // 31536000 seconds in a year
 
